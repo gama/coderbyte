4671170dbd27414819f8890c5b77ecb16cfd40af
Gustavo Gama <gustavo.gama@gmail.com>
2019-03-03 02:45:56 -0300

    Add VowelSquare challenge from coderbyte

--- vowel-square/index.js
+++ vowel-square/index.js
@@ -14,14 +14,37 @@
 // and first column, namely, ei, ou. If a 2x2 square of vowels is found your
 // program should return the top-left position (row-column) of the square, so
 // for this example your program should return 1-0. If no 2x2 square of vowels
-// exists, then return the string not found. If there are multiple squares of
+// exists, then return the string "not found". If there are multiple squares of
 // vowels, return the one that is at the most top-left position in the whole
 // matrix. The input matrix will at least be of size 2x2. 
 
 module.exports = VowelSquare
 
 function VowelSquare(strArray) {
-    return strArray
+    let charMatrix = strArray.map(str => [...str])
+    for (let square of enumerateSquares(charMatrix))
+        if (allVowels(square))
+            return square.id
+    return 'not found'
+}
+
+function enumerateSquares(charMatrix) {
+    let squares = []
+    for (let i = 0; i < charMatrix.length - 1; ++i)
+        for (let j = 0; j < charMatrix[i].length - 1; ++j) {
+            const square = [
+                [charMatrix[i][j],   charMatrix[i][j+1]],
+                [charMatrix[i+1][j], charMatrix[i+1][j+1]],
+            ]
+            square.id = `${i}-${j}`
+            squares.push(square)
+        }
+    return squares
+}
+
+const VOWELS = ['a', 'e', 'i', 'o', 'u']
+function allVowels(square) {
+    return square.every(row => row.every(el => VOWELS.includes(el)))
 }
 
 if (module === require.main)
--- vowel-square/test.js
+++ vowel-square/test.js
@@ -3,6 +3,10 @@ const assert      = require('assert')
 const VowelSquare = require('.')
 
 test('vower square', () => {
-    assert(VowelSquare(['aqrst', 'ukaei', 'ffooo']) === '1-2')
+    assert(VowelSquare(['uu', 'ea'])                === '0-0')
+    assert(VowelSquare(['bu', 'ea'])                === 'not found')
+    assert(VowelSquare(['uu', 'ez'])                === 'not found')
     assert(VowelSquare(['gg', 'ff'])                === 'not found')
+    assert(VowelSquare(['aqrst', 'ukaei', 'ffooo']) === '1-2')
+    assert(VowelSquare(['zmnboni', 'qxrqopz', 'bfoaaok', 'oxbduil']) === '2-4')
 })
