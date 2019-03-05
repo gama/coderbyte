435e8fc36fa122838ae420738f50ba40814d59c5
Gustavo Gama <gustavo.gama@gmail.com>
2019-03-04 22:46:05 -0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "amd":     true,
+        "node":    true,
+        "jest":    true,
+        "es6":     true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "settings": {
+        "react": {
+            "version": "16.0"
+        }
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ],
+        "no-console": "off",
+        "react/prop-types": "off"
+    }
+};
---
+++ README.md
@@ -0,0 +1,4 @@
+# Coderbyte Coding Challenges
+
+Solutions to the free coding challenges published by
+[Coderbyte](https://coderbyte.com).
