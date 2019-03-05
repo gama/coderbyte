7b7a75b23801637d8e971b992de2bc55c396493d
Gustavo Gama <gustavo.gama@gmail.com>
2019-03-04 22:28:02 -0300

    Add EightQueens challenge from coderbyte

--- eight-queens/index.js
+++ eight-queens/index.js
@@ -8,7 +8,7 @@
 // 1,1 is the bottom-left of the chessboard and 8,8 is the top-right). Your
 // program should determine if all of the queens are placed in such a way where
 // none of them are attacking each other. If this is true for the given input,
-// return the string true otherwise return the first queen in the list that is
+// return the string "true" otherwise return the first queen in the list that is
 // attacking another piece in the same format it was provided.
 // 
 // For example: if strArr is ["(2,1)", "(4,2)", "(6,3)", "(8,4)", "(3,5)",
@@ -19,8 +19,30 @@
 module.exports = EightQueens
 
 function EightQueens(strArray) {
-    return strArray
+    const queens = strArray.map((strPos) => {
+        const [, x, y] = strPos.match(/\((\d),(\d)\)/)
+        return {x, y}
+    })
+
+    for (let queen of queens)
+        if (queens.some(q => attacks(queen, q)))
+            return `(${queen.x},${queen.y})`
+    return 'true'
 }
 
+function attacks(queen1, queen2) {
+    return (queen1 !== queen2) && (
+        sameVertical(queen1, queen2)    ||
+        sameHorizontal(queen1, queen2)  ||
+        same45Diagonal(queen1, queen2)  ||
+        same135Diagonal(queen1, queen2)
+    )
+}
+
+const sameVertical    = (q1, q2) => q1.y === q2.y
+const sameHorizontal  = (q1, q2) => q1.x === q2.x
+const same45Diagonal  = (q1, q2) => q1.x + q1.y === q2.x + q2.y
+const same135Diagonal = (q1, q2) => q1.x - q1.y === q2.x - q2.y
+
 if (module === require.main)
-    console.log('eight queens: ', EightQueens(process.argv[2]))
+    console.log('eight queens: ', JSON.parse(EightQueens(process.argv[2])))
